558939e62e46e94bc1e18cc5369402933800ac8b
N00byEdge <hannesbredberg@gmail.com>
2022-02-22 18:46:54 +0100

    Zig compiler update

--- build.zig
+++ build.zig
@@ -1,62 +1,5 @@
 const std = @import("std");
 
-fn takes_new_file_ref() bool {
-    return @typeInfo(@TypeOf(std.build.LibExeObjStep.setLinkerScriptPath)).Fn.args[1].arg_type.? != []const u8;
-}
-
-fn file_ref(filename: []const u8) if (takes_new_file_ref()) std.build.FileSource else []const u8 {
-    if (comptime takes_new_file_ref()) {
-        return .{ .path = filename };
-    } else {
-        return filename;
-    }
-}
-
-const TransformFileCommandStep = struct {
-    step: std.build.Step,
-    output_path: []const u8,
-    fn run_command(_: *std.build.Step) !void {}
-};
-
-fn make_transform(b: *std.build.Builder, dep: *std.build.Step, command: [][]const u8, output_path: []const u8) !*TransformFileCommandStep {
-    const transform = try b.allocator.create(TransformFileCommandStep);
-
-    transform.output_path = output_path;
-    transform.step = std.build.Step.init(.custom, "", b.allocator, TransformFileCommandStep.run_command);
-
-    const command_step = b.addSystemCommand(command);
-
-    command_step.step.dependOn(dep);
-    transform.step.dependOn(&command_step.step);
-
-    return transform;
-}
-
-fn section_blob(b: *std.build.Builder, elf: *std.build.LibExeObjStep, section_name: []const u8) !*TransformFileCommandStep {
-    const elf_path = b.getInstallPath(elf.install_step.?.dest_dir, elf.out_filename);
-
-    const dumped_path = b.fmt("{s}.bin", .{elf_path});
-
-    return try make_transform(
-        b,
-        &elf.install_step.?.step,
-        &[_][]const u8{
-            // zig fmt: off
-            "llvm-objcopy",
-                "-O", "binary",
-                "--only-section", section_name,
-                elf_path, dumped_path,
-            // zig fmt: on
-        },
-        dumped_path,
-    );
-}
-
-fn blob(b: *std.build.Builder, elf: *std.build.LibExeObjStep) !*TransformFileCommandStep {
-    elf.install();
-    return section_blob(b, elf, ".blob");
-}
-
 const proto_pkg = std.build.Pkg{
     .name = "proto",
     .path = .{ .path = "src/proto/proto.zig" },
@@ -79,9 +22,11 @@ const host_pkg = std.build.Pkg{
 
 fn buildArmHostEL(b: *std.build.Builder, el: usize) !void {
     const exec = b.addExecutable(b.fmt("host_aarch64_EL{d}", .{el}), "src/host/arch/aarch64/main.zig");
-    exec.addBuildOption(usize, "debugger_el", el);
+    const options = b.addOptions();
+    options.addOption(usize, "debugger_el", el);
+    exec.addPackage(options.getPackage("build_options"));
     exec.code_model = .tiny;
-    exec.setLinkerScriptPath(file_ref("src/host/arch/aarch64/linker.ld"));
+    exec.setLinkerScriptPath(.{.path = "src/host/arch/aarch64/linker.ld"});
     exec.setMainPkgPath("src/");
     exec.setBuildMode(.ReleaseSmall);
     exec.addPackage(host_pkg);
@@ -105,7 +50,15 @@ fn buildArmHostEL(b: *std.build.Builder, el: usize) !void {
         .cpu_features_add = enabled_feautres,
     });
 
-    const blob_step = try blob(b, exec);
+    exec.install();
+
+    const blob_step = exec.installRaw(b.fmt("host_aarch64_EL{d}.bin", .{el}), .{
+        .format = .bin,
+        .only_section_name = ".blob", 
+        .pad_to_size = 0x1000,
+    });
+
+    b.default_step.dependOn(&exec.install_step.?.step);
     b.default_step.dependOn(&blob_step.step);
 
     const build_step = b.step(b.fmt("host-aarch64-EL{d}", .{el}), b.fmt("Build the aarch64 host for EL{d}", .{el}));
